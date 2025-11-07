c1a9caf70dee9c8d5ab7a9c233efb4afa38f4695
Michael Langbein <michael_langbein@hotmail.de>
2025-11-07 21:35:43 +0100

    switched from swe to spring equations again, more stable

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,6 @@
 import { getWebGpuContext } from './lib/utils';
 
 /**
- * TODO:
- * - Turn ship positions into meters (f32)
- * - Move away from swe, towards springs
  *
  */
 
@@ -16,26 +13,29 @@ const heightPx = 480;
 const widthM = 75;
 const heightM = 50;
 
-const metaData = {
-  width: widthM, // scene width in m
-  height: heightM, // scene height in m
-  deltaX: widthM / widthPx, // = width / nrPixelsWidth
-  deltaY: heightM / heightPx, // = height / nrPixelsHeight
-  deltaT: 0.0001, // should not be much bigger than 0.01
-};
 
 const shipPositions = [
   {
-    x: 0.5,
-    y: 0.5,
+    xM: widthM / 2,
+    yM: heightM / 2,
     rotation: 0.0,
   },
   {
-    x: 0.25,
-    y: 0.25,
+    xM: widthM / 4,
+    yM: heightM / 4,
     rotation: Math.PI,
   },
 ];
+
+const metaData = {
+  widthM: widthM, // scene width in m
+  heightM: heightM, // scene height in m
+  deltaX: widthM / widthPx, // = width / nrPixelsWidth
+  deltaY: heightM / heightPx, // = height / nrPixelsHeight
+  deltaT: 0.01, // should not be much bigger than 0.01
+  shipCount: shipPositions.length
+};
+
 
 const canvas = document.querySelector('#canvas') as HTMLCanvasElement;
 canvas.width = widthPx;
@@ -51,16 +51,17 @@ const waterShader = device.createShaderModule({
   code: /*wgsl*/ `
 
         struct MetaData {
-            width: f32,   // scene width in m
-            height: f32,  // scene height in m
+            widthM: f32,   // scene width in m
+            heightM: f32,  // scene height in m
             deltaX: f32,  // = width / nrPixelsWidth
             deltaY: f32,  // = height / nrPixelsHeight
             deltaT: f32,  // should not be much bigger than 0.01
+            shipCount: f32  // only a float for consistency
         }
 
         struct ShipPosition {
-            x: f32,
-            y: f32,
+            xM: f32,
+            yM: f32,
             rotation: f32
         }
 
@@ -88,14 +89,16 @@ const waterShader = device.createShaderModule({
             vec2(1.0, 0.0)  // Top-right pos
         );
 
-        const g = 9.81;   // m/s^2
-        const H = 1.0;    // bathymetry
-        const k = 0.0;    // viscous drag of water
-        const f = 0.523;  // coriolis coefficient
-
         @group(0) @binding(0) var<uniform> metaData: MetaData;
         @group(0) @binding(1) var<storage, read> shipPositions: array<ShipPosition>;
-        @group(0) @binding(2) var huvTexture: texture_2d<f32>;
+        @group(0) @binding(2) var vhTexture: texture_2d<f32>;
+
+        fn worldCoordsToUv(worldCoords: vec2f) -> vec2f {
+          return vec2f(
+            worldCoords.x / metaData.widthM,
+            worldCoords.y / metaData.heightM
+          );
+        }
 
         // some textures cannot be sampled with 'textureSample', for instance r32f or rgba32f.
         // so we use 'textureLoad' instead.
@@ -132,39 +135,43 @@ const waterShader = device.createShaderModule({
             let dx = metaData.deltaX;
             let dy = metaData.deltaY;
             let dt = metaData.deltaT;
-            let width = metaData.width;
-            let height = metaData.height;
+            let width = metaData.widthM;
+            let height = metaData.heightM;
+            let delta = worldCoordsToUv(vec2f(dx, dy));
 
-            let huv = myTextureSampler(huvTexture, vo.uv);
-            let huv_xp = myTextureSampler(huvTexture, vo.uv + vec2f(dx/width, 0.0));
-            let huv_yp = myTextureSampler(huvTexture, vo.uv + vec2f(0.0, dy/height));
+            let vh = myTextureSampler(vhTexture, vo.uv);
+            let vh_xp = myTextureSampler(vhTexture, vo.uv + vec2f(delta.x, 0));
+            let vh_xm = myTextureSampler(vhTexture, vo.uv - vec2f(delta.x, 0));
+            let vh_yp = myTextureSampler(vhTexture, vo.uv + vec2f(0, delta.y));
+            let vh_ym = myTextureSampler(vhTexture, vo.uv - vec2f(0, delta.y));
 
-            var h = huv[0];
-            let u = huv[1];
-            let v = huv[2];
-            let h_xp = huv_xp[0];
-            let u_xp = huv_xp[1];
-            let h_yp = huv_yp[0];
-            let v_yp = huv_yp[2];
+            let v1 = vh.x;
+            let h1 = vh.y;
+            let h1_xp = vh_xp.y;
+            let h1_xm = vh_xm.y;
+            let h1_yp = vh_yp.y;
+            let h1_ym = vh_ym.y;
+
+            // water simulation
+            var v2 = v1 + dt * ( h1_xp + h1_xm + h1_yp + h1_ym - 4.0 * h1 ) / (dx * dy);
+            v2 *= 0.99;
+            var h2 = h1 + dt * v2;
+
 
             // if this is a ship location, create some distortion
-            for (var s: u32 = 0; s < 2; s++) {
-                let shipPos = shipPositions[0];
-                let dir = vo.uv - vec2f(shipPos.x, shipPos.y);
+            for (var s: u32 = 0; s < u32(metaData.shipCount); s++) {
+                let shipPosM = shipPositions[s];
+                let shipPosUv = worldCoordsToUv(vec2f(shipPosM.xM, shipPosM.yM));
+                let dir = vo.uv - shipPosUv;
                 let dist = length(dir);
                 if (dist < 0.01) {
-                    h += 0.01;
+                    h2 += 0.01;
                 }
             }
 
-            // shallow water equations
-            let h_tp = - H * ( (u_xp - u)/dx  +  (v_yp - v)/dy ) * dt + h;
-            let u_tp = ( -g * (h_xp - h)/dx  - k*u + f*v ) * dt + u;
-            let v_tp = ( -g * (h_yp - h)/dy  - k*v - f*u ) * dt + v;
-
             var fo = FragmentOutput();
-            fo.huvOutput = vec4f(h_tp, u_tp, v_tp, 1.0);
-            fo.diffuseColor = interpolateColor(h_tp);
+            fo.huvOutput = vec4f(v2, h2, 0, 1.0);
+            fo.diffuseColor = interpolateColor(h2);
             return fo;
         }
 
@@ -172,11 +179,12 @@ const waterShader = device.createShaderModule({
 });
 
 const metaDataArray = new Float32Array([
-  metaData.width,
-  metaData.height,
+  metaData.widthM,
+  metaData.heightM,
   metaData.deltaX,
   metaData.deltaY,
   metaData.deltaT,
+  metaData.shipCount
 ]);
 const metaDataBuffer = device.createBuffer({
   label: 'metadata',
@@ -186,11 +194,11 @@ const metaDataBuffer = device.createBuffer({
 device.queue.writeBuffer(metaDataBuffer, 0, metaDataArray, 0);
 
 const shipPosArray = new Float32Array([
-  shipPositions[0].x,
-  shipPositions[0].y,
+  shipPositions[0].xM,
+  shipPositions[0].yM,
   shipPositions[0].rotation,
-  shipPositions[1].x,
-  shipPositions[1].y,
+  shipPositions[1].xM,
+  shipPositions[1].yM,
   shipPositions[1].rotation,
 ]);
 const shipPosBuffer = device.createBuffer({
@@ -200,15 +208,15 @@ const shipPosBuffer = device.createBuffer({
 });
 device.queue.writeBuffer(shipPosBuffer, 0, shipPosArray, 0);
 
-const huvTexture1 = device.createTexture({
-  label: 'huvTexture1',
+const vhTexture1 = device.createTexture({
+  label: 'vhTexture1',
   format: 'rgba32float',
   size: [widthPx, heightPx],
   usage: GPUTextureUsage.TEXTURE_BINDING | GPUTextureUsage.RENDER_ATTACHMENT,
 });
 
-const huvTexture2 = device.createTexture({
-  label: 'huvTexture2',
+const vhTexture2 = device.createTexture({
+  label: 'vhTexture2',
   format: 'rgba32float',
   size: [widthPx, heightPx],
   usage: GPUTextureUsage.TEXTURE_BINDING | GPUTextureUsage.RENDER_ATTACHMENT,
@@ -227,7 +235,7 @@ const waterPipeline = device.createRenderPipeline({
         format,
       },
       {
-        format: huvTexture1.format,
+        format: vhTexture1.format,
       },
     ],
   },
@@ -246,7 +254,7 @@ const waterBindgroup1 = device.createBindGroup({
     },
     {
       binding: 2,
-      resource: huvTexture1.createView(),
+      resource: vhTexture1.createView(),
     },
   ],
 });
@@ -264,7 +272,7 @@ const waterBindgroup2 = device.createBindGroup({
     },
     {
       binding: 2,
-      resource: huvTexture2.createView(),
+      resource: vhTexture2.createView(),
     },
   ],
 });
@@ -284,7 +292,7 @@ function render() {
       {
         loadOp: 'load',
         storeOp: 'store',
-        view: i % 2 === 0 ? huvTexture2.createView() : huvTexture1.createView(),
+        view: i % 2 === 0 ? vhTexture2.createView() : vhTexture1.createView(),
       },
     ],
   });
